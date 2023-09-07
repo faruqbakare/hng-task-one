71e924af7eeb506502af097d9f263e31b52028dd
Bakare Olasubomi <bakareolasubomi13@gmail.com>
2023-09-07 20:53:20 +0100

    task commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const now = new Date()
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const dayOfWeek = daysOfWeek[now.getDay()]
+const hours = now.getHours()
+const minutes = now.getMinutes()
+let PmNoon = null
+
+if (hours => 12){
+    PmNoon = 'PM'
+}else{
+    PmNoon = 'AM'
+}
+
+let day = document.getElementById('day')
+day.innerHTML = `Its ${dayOfWeek} Today!`
+let time = document.getElementById("time")
+time.innerHTML = `The time is ${hours}:${minutes} ${PmNoon}`
